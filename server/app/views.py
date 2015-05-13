cc8b33a01fc556e4c232f9ff3f44a55cd47f9d6e
Phil Wheldon <philip.wheldon@nhs.net>
2015-05-13 21:51:01 +0000

    Download epub with single word replacement

--- server/app/views.py
+++ server/app/views.py
@@ -1,4 +1,4 @@
-import os
+import os, cStringIO, zipfile
 from django.http import HttpResponse
 from django.core.servers.basehttp import FileWrapper
 
@@ -8,9 +8,26 @@ def index(request):
     return HttpResponse("Hello, world!")
     
 def getBook(request):
+    response = HttpResponse(content_type='application/zip')
+    response['Content-Disposition'] = 'filename="book.epub"'
     fileName = os.path.join(APP_DIR, '../files/greatGatsby.epub')
-    wrapper = FileWrapper(file(fileName))
-    response = HttpResponse(wrapper, content_type='text/plain')
-    response['Content-Length'] = os.path.getsize(fileName)
+    
+    buffer = cStringIO.StringIO()
+    zip = zipfile.ZipFile(buffer, "w", zipfile.ZIP_DEFLATED)
+    
+    with zipfile.ZipFile(fileName, 'r') as originalBook:
+        for fileEntryName in originalBook.namelist():
+            with originalBook.open(fileEntryName) as fileEntry:
+                if "main" in fileEntryName:
+                    zip.writestr(fileEntryName, fileEntry.read().replace("Gatsby", "Phil"))
+                else:
+                    zip.writestr(fileEntryName, fileEntry.read())
+    
+    zip.close()
+    buffer.flush()
+
+    ret_zip = buffer.getvalue()
+    buffer.close()
+    response.write(ret_zip)
     return response
     
