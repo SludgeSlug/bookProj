70fac6d205b9b310ece3c1d6ea514eadb32b897a
SludgeSlug <philip.wheldon@googlemail.com>
2015-05-14 16:24:12 +0000

    Further changes for pythonanywhere

--- server/app/bookMaker.py
+++ server/app/bookMaker.py
@@ -5,34 +5,36 @@ APP_DIR = os.path.dirname(os.path.abspath(__file__))
 #TEST JSON = '[ { "replace": "Gatsby", "with": "Phil" }, { "replace": "man", "with": "fish" } ]'
 
 class BookMaker:
-    
+
     def __init__(self, replacementWords):
         self.replacementWords = replacementWords
-    
+
     def getZipStream(self):
         fileName = os.path.join(APP_DIR, '../files/greatGatsby.epub')
-    
-        buffer = io.StringIO()
+
+        buffer = io.BytesIO()
         zip = zipfile.ZipFile(buffer, "w", zipfile.ZIP_DEFLATED)
-        
+
         with zipfile.ZipFile(fileName, 'r') as originalBook:
             for fileEntryName in originalBook.namelist():
                 with originalBook.open(fileEntryName) as fileEntry:
                     if "main" in fileEntryName:
                         zip.writestr(fileEntryName, self.getFileContents(fileEntry.read()))
                     else:
-                        zip.writestr(fileEntryName, fileEntry.read())
-        
+                        contents = fileEntry.read()
+                        zip.writestr(fileEntryName, contents)
+
         zip.close()
         buffer.flush()
-    
+
         ret_zip = buffer.getvalue()
         buffer.close()
-        
+
         return ret_zip
-        
+
     def getFileContents(self, fileContents):
+        replWords = self.replacementWords.decode("utf-8")
         newFileContents = fileContents
-        for replaceEntry in json.loads(self.replacementWords):
+        for replaceEntry in json.loads(replWords):
             newFileContents = newFileContents.replace(replaceEntry['replace'].encode("ascii"), replaceEntry['with'].encode("ascii"))
         return newFileContents
