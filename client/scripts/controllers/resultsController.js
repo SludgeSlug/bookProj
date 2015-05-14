7eb3e1d26ea090e2e5513d659acfaef2119220c4
Phil Wheldon <philip.wheldon@nhs.net>
2015-05-14 18:41:37 +0000

    Post JSON to API

--- client/scripts/controllers/resultsController.js
+++ client/scripts/controllers/resultsController.js
@@ -5,13 +5,29 @@
         .module('app')
         .controller('ResultsController', ResultsController);
         
-    ResultsController.$inject = ['$scope', 'WordService'];
+    ResultsController.$inject = ['$scope', 'WordService', '$http'];
     
-    function ResultsController(scope, wordService) {
+    function ResultsController(scope, wordService, http) {
         
         var resultsCtrl = this;
         
         scope.words = wordService.words;
+        
+        resultsCtrl.download = function() {
+            
+            var request = http({
+                method: 'post',
+                url: 'https://sludgeslug.pythonanywhere.com/api/book/',
+                data: wordService.words
+            });
+            
+            request.success(function(data, status, headers, config) {
+    			
+    		});
+    		request.error(function(data, status, headers, config) {
+    			alert( "failure message: " + JSON.stringify({data: data}));
+    		});	
+        }
     };
     
 })();
